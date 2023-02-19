2d7fb6963a09d16a704a3aa39923c493b3eb2e7b
action0709 <action0709@mail.ru>
2023-02-19 17:02:22 +0400

    задача 1,2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        task1();
+        task2();
     }
-}
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        // Пишем код для задачи 1
+        int intVariable = 2000000;
+        byte byteVariable =110;
+        short shortVariable =-30500;
+        long longVariable =1100000000L;
+        float floatVariable =3;
+        double  doubleVariable =-110.45;
+        System.out.println("Значение переменной intVariable c типом int равно "+intVariable);
+        System.out.println("Значение переменной byteVariable c типом byte равно "+byteVariable);
+        System.out.println("Значение переменной shortVariable c типом short равно "+shortVariable);
+        System.out.println("Значение переменной longVariable c типом long равно "+longVariable);
+        System.out.println("Значение переменной floatVariable c типом float равно "+floatVariable);
+        System.out.println("Значение переменной doubleVariable c типом double равно "+doubleVariable);
+
+    }
+
+    public static void task2 (){
+         System.out.println("Задача 2");
+    // Пишем код для задачи 2
+   float f=27.12f;
+   long l=987678965549L;
+   float f1=2.786f;
+   short s=569;
+   short s1=-159;
+   short s3=27897;
+   int i=67;
+
+}}
